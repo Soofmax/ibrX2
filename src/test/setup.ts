0fcef30fb3b47dfe9257f95e997818cf95666367
bergagax <bergagax@gmail.com>
2025-10-08 23:07:02 +0000

    chore: extend vitest with Testing Library matchers
    Co-authored-by: Genie <genie@cosine.sh>

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,10 @@
 import { expect } from 'vitest';
 import matchers from '@testing-library/jest-dom/matchers';
+import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
+  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<any, void> {}
+}
