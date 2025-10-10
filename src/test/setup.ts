e6aa671e36a41a40bd85bdca95d338f9bf3ffe72
bergagax <bergagax@gmail.com>
2025-10-10 20:10:01 +0000

    fix: allow any type in custom assertion interfaces
    Co-authored-by: Genie <genie@cosine.sh>

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,8 +5,8 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 expect.extend(matchers);
 
 declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Assertion<T = unknown> extends TestingLibraryMatchers<T, void> {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<unknown, void> {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
+  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
+  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<any, void> {}
 }
