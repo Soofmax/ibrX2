597882690cee1e951dd8a6647c3fc74314640fbe
bergagax <bergagax@gmail.com>
2025-10-10 20:15:03 +0000

    chore: simplify test setup by importing jest-dom directly
    Co-authored-by: Genie <genie@cosine.sh>

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,12 +1,1 @@
-import { expect } from 'vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
-import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-
-expect.extend(matchers as unknown as Record<string, unknown>);
-
-declare module 'vitest' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
-  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
-  interface AsymmetricMatchersContaining extends TestingLibraryMatchers<any, void> {}
-}
+import '@testing-library/jest-dom/vitest';
